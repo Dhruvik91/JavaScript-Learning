e1af65301130876b85053b405e2f8e3eba59ce8a
Dhruvik Gondaliya <trynew008@gmail.com>
2023-07-28 16:16:23 +0400

    worked with new method in class

--- 8.5_Extending_built-in-classes.js
+++ 8.5_Extending_built-in-classes.js
@@ -8,5 +8,22 @@ class NewArray extends Array {   // here there is no methods of class "NewArray"
 
 }
 
-let array = new NewArray([8,[6,1],4,7]);
-console.log(array.flat());
+let array = new NewArray([8,[6,1],4,7]); 
+console.log(array.flat());   // this will work as "Array" has the defaults methods
+
+/* let's create a new class */
+
+class NewString extends String {
+
+    constructor (str) {
+        super();
+        this.str = str;
+    }
+    newMethod() {                              // this is a method of the child class "NewString"
+        console.log("this is new string ");
+    }
+}
+
+let astring = new NewString("HELLO");
+console.log(astring.toLowerCase()); // this both will work but without the construtor of the child class 
+console.log(astring.newMethod());
