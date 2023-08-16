cc349be879bc1658b06e8aaf791d76140ddd6e2f
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-16 17:04:08 +0400

    Add data types

---
+++ 2.0 JS Fundamentals/2.3 Data Types/DataTypes.js
@@ -0,0 +1,19 @@
+/* What is the output of the script?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
+
+*/
+
+/* The output will be:-
+
+hello 1
+hello name
+hello Ilya
+
+*/
