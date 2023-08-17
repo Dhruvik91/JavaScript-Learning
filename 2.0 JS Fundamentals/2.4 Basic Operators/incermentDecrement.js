027bc3b9028218468b5a0a7400fbfa77111e8490
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-17 11:24:20 +0400

    Learn about incer/decre operator

---
+++ 2.0 JS Fundamentals/2.4 Basic Operators/incermentDecrement.js
@@ -0,0 +1,16 @@
+/* In this script, we are gooing to learn about incerementand decrement operators */
+
+// prefix 
+
+let counter = 1;
+let a = ++counter; // (*)
+
+alert(a); // 2
+
+// postfix
+
+let counter1 = 1;
+let b = counter1++; // (*) // here the postfix operator increse the value but give the old value  
+
+alert(b); // 1
+
