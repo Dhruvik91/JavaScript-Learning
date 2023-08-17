21a415f0b33825fd8b731efeed0256598b61bae6
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-17 11:33:53 +0400

    Adding tasks of operators

---
+++ 2.0 JS Fundamentals/2.4 Basic Operators/task1.js
@@ -0,0 +1,6 @@
+/* What are the final values of all variables a, b, c and d after the code below? */
+
+let a = 1, b = 1;
+
+let c = ++a; // 2
+let d = b++; // 1 
---
+++ 2.0 JS Fundamentals/2.4 Basic Operators/task2.js
@@ -0,0 +1,10 @@
+/* What are the values of a and x after the code below?
+
+let a = 2;
+
+let x = 1 + (a *= 2); */
+
+
+let x = 1 + (a *= 2);
+
+console.log(x);  // 5
