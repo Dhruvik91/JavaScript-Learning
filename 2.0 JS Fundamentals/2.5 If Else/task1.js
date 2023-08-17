f8d826d77e7fb3417ca225365322741009a37a59
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-17 12:11:28 +0400

    Add new task about if Else

---
+++ 2.0 JS Fundamentals/2.5 If Else/task1.js
@@ -0,0 +1,11 @@
+/* Rewrite this if using the conditional operator '?':
+
+let result;
+
+if (a + b < 4) {
+  result = 'Below';
+} else {
+  result = 'Over';
+} */
+
+let result = (a + b < 4) ? alert("Below") : alert("Over");
