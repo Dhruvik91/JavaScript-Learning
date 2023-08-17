71e2d3bca6989f8f3ec1a7032c043369b76c2515
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-17 17:00:59 +0400

    Learn about for loop

---
+++ 2.0 JS Fundamentals/2.6 Loops/forLoop.js
@@ -0,0 +1,8 @@
+/* Here is the code for the "for" loop */
+
+let counter = 0;
+
+for (let i = 0; i < 5; i++) {
+
+     console.log(counter++);
+}
---
+++ 2.0 JS Fundamentals/2.6 Loops/task1.js
@@ -0,0 +1,9 @@
+/* Use the for loop to output even numbers from 2 to 10. */
+
+
+for (let i = 2; i < 11; i++) {
+
+    if ( i % 2 == 0) {
+       console.log(i);
+    }
+}
