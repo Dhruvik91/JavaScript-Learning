aa742adde87248b4b5e516464ea9a5dc2f828c40
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-19 13:50:35 +0400

    Learn about console.table()

---
+++ FreeCodeCampPractice/Learn Advanced JS/TIp 1/Tip1.js
@@ -0,0 +1,10 @@
+/* Tip 1: Use "console.table()" to Display Arrays and Objects in the Console */
+
+/* console.log will display the array or object as usual,
+ but console.table will tabularize the index and values for you. */
+
+
+ const myArr = ['Kolade', 'Chelsea', 10, true];
+
+console.log(myArr);  // it will display the data as usual  
+console.table(myArr);  // it will display the data into index and value form.
