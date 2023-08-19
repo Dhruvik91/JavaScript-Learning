3f9f2424f979e25ec563348dfee542d6a8d113e0
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-19 14:06:34 +0400

    Learn about console.group()

---
+++ FreeCodeCampPractice/Learn Advanced JS/Tip 3/tip3.js
@@ -0,0 +1,9 @@
+/* Tip 5: Use console.group() with Multiple console.log()s to Group Related Items to the Console */
+
+console.group('Bio:');
+
+console.log('My name is Kolade');
+console.warn("I don't like to be late");
+console.error('You came late');
+
+console.groupEnd();
