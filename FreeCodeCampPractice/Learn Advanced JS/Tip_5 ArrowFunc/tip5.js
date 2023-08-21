8cb53ad383baa8fc6563468373eed7d48033d7db
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-21 15:52:27 +0400

    Add the arrow function

---
+++ FreeCodeCampPractice/Learn Advanced JS/Tip_5 ArrowFunc/tip5.js
@@ -0,0 +1,11 @@
+/* Tip 5: The arrow function is used to make the function easy */
+
+function add1(a, b) {
+    return a + b;
+}
+
+let add2 = (a, b) => a + b;
+
+console.log(add1(4, 5));
+console.log(add2(1, 2));
+
