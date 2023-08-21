6c1ac515af673ae4fac59243f85a93cdc16c79b6
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-08-21 16:01:56 +0400

    Learn new strings methods

---
+++ FreeCodeCampPractice/Learn Advanced JS/TIp_6 startsWith() and endsWith()/tip6.js
@@ -0,0 +1,23 @@
+/* Tip 6: The startsWith() and endsWith() methods help you determine if a string begins or ends with a specific substring. 
+
+Since they both return true or false, they are handy for various string manipulations and conditional checks. */
+
+const message = 'Hello world';
+
+console.log(message.startsWith('H')); // true
+console.log(message.startsWith('h')); // false
+console.log(message.endsWith('d')); // true
+console.log(message.endsWith('D')); // false
+
+const files = [
+    'text.txt',
+    'document.txt',
+    'image.jpg',
+    'script.js',
+    'docs.txt',
+  ];
+  
+  // Get .txt files
+  const textFiles = files.filter((file) => file.endsWith('.txt'));
+  console.log(textFiles); // [ 'text.txt', 'document.txt', 'docs.txt' ]
+
