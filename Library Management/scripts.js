38985f329ce2f3d46a5943d1d206f45631c02b8a
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-09-02 00:22:32 +0400

    Add the functions

--- Library Management/scripts.js
+++ Library Management/scripts.js
@@ -1,41 +1,51 @@
-
-// this is the array of object, which stores the books data
+// this is the array of objects, which stores the books' data
 let library = [];
 
-
-// this function create the object of book data 
-function createBook(title, author, isbn) {  
-    
+// this function creates an object for book data
+function createBook(title, author, bookISBN) {  
     const book = {
         Title: title,
         Author: author,
-        ISBN: isbn,
+        ISBN: bookISBN,
         checkedOut: false
     }
-      return book;
+    return book;
 }
 
-
-// this function add the book to the library
+// this function adds the book to the library
 function addToLibrary(book) {
-
     library.push(book);
     return library;
-    
-} 
-
-
-const book1 = (createBook("The story", "Ravi", 12344));
-console.log(addToLibrary(book1));
-
-
-
-function checkedOutBook(isbn) {
-  
-    const oldIsbn = library.map(book => book.ISBN);
-   
-    if ( isbn === oldIsbn) {
-        
-    }
-
 }
+
+const book1 = createBook("The story", "Ravi", 12344);
+const book2 = createBook("The story of him", "Ravinath", 23456);
+const book3 = createBook("The story of her", "Ravisankar", 45646);
+
+addToLibrary(book1);
+addToLibrary(book2);
+addToLibrary(book3);
+
+// this function is used to check out a book
+function checkedOutBook(isbn) {
+    library.forEach((book) => {
+        if (book.ISBN === isbn) {
+            book.checkedOut = true; // Set the checkedOut key of the matched book to true
+        }
+    });
+}
+
+checkedOutBook(23456);
+console.log(library);
+
+function returnBook(isbn) {
+    library.forEach((book) => {
+        if (book.ISBN === isbn) {
+            book.checkedOut = false // Set the checkedOut key of the matched book to false 
+        }
+    });
+}
+
+returnBook(12344);
+
+console.log(library);
