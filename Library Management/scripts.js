3ad2819c71e9ddbbfc8d041f75749cd5f9da4e46
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-09-02 11:46:26 +0400

    Add the list of overduedate function

--- Library Management/scripts.js
+++ Library Management/scripts.js
@@ -1,77 +1,69 @@
-// this is the array of objects, which stores the books' data
 let library = [];
-
-let count = 0;
-
 const MAX_COUNT = 3;
 
-// this function creates an object for book data
 function createBook(title, author, bookISBN) {
     const book = {
         Title: title,
         Author: author,
         ISBN: bookISBN,
         checkedOut: false,
-        checkedCount: count,
-    }
+        checkedCount: 0,
+        dueDate: null,
+    };
     return book;
 }
 
-// this function adds the book to the library
 function addToLibrary(book) {
     library.push(book);
     return library;
 }
 
-
-
-// this function is used to check out a book
 function checkedOutBook(isbn, noOfDays) {
+    const book = library.find((book) => book.ISBN === isbn);
 
-    const now = new Date();
-    const dueDate = new Date(now.getTime() + (noOfDays * 24 * 60 * 60 * 1000));
-
-    library.forEach((book) => {
-        if (book.ISBN === isbn) {
-            book.checkedOut = true;      // Set the checkedOut key of the matched book to true
-            book.checkedCount = ++count;
-            book.dueDate = dueDate;
-
-            if (book.checkedCount > MAX_COUNT) {
-                book.checkedOut = false;       
-                alert("This book is unavaiable");
-            }
-        }
-    });
+    if (!book) {
+        alert("Book not found in the library.");
+    } else if (book.checkedCount > MAX_COUNT) {
+        alert("This book is unavailable for checkout.");
+    } else if (book.checkedOut) {
+        alert("This book is already checked out.");
+    } else {
+        book.checkedOut = true;
+        book.checkedCount++;
+        const dueDate = new Date();
+        dueDate.setDate(dueDate.getDate() + noOfDays);
+        book.dueDate = dueDate;
+        console.log(`Checked out: ${book.Title}, Due Date: ${dueDate}`);
+    }
 }
-
 
 function returnBook(isbn) {
-    library.forEach((book) => {
-        if (book.ISBN === isbn) {
-            book.checkedOut = false // Set the checkedOut key of the matched book to false 
-        }
-    });
+    const book = library.find((book) => book.ISBN === isbn);
+    if (book) {
+        book.checkedOut = false;
+    }
 }
 
-
-let bookWritten = []; // this is the array that will add the books by the author namw
-
-
-// this function find the book wirtten by the author
 function findBookByAuthor(authorName) {
-
+    const booksByAuthor = [];
     library.forEach((book) => {
         if (book.Author === authorName) {
-            bookWritten.push(book.Title);
+            booksByAuthor.push(book.Title);
         }
     });
-    return bookWritten;
+    return booksByAuthor;
 }
 
-
-
-
+function listOverDueDate() {
+    const overdueBooks = [];
+    const now = new Date();
+    library.forEach((book) => {
+        if (book.dueDate && book.dueDate < now) {
+            overdueBooks.push(book.Title);
+        }
+    });
+    return overdueBooks;
+}
 
 const book1 = createBook("The story", "Ravi", 12344);
 const book2 = createBook("The story of him", "Ravinath", 23456);
@@ -83,11 +75,12 @@ addToLibrary(book2);
 addToLibrary(book3);
 addToLibrary(book4);
 
-checkedOutBook(23456, 4);
+checkedOutBook(23456, 1);
 console.log(library);
 
 returnBook(12344);
 console.log(library);
 
-
 console.log(findBookByAuthor("Ravi"));
+
+console.log(listOverDueDate());
