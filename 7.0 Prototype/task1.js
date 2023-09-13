42e31ef198a06bd47a82f65884a5a1f68d101ddd
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-09-13 15:21:37 +0400

    Learn the proto

---
+++ 7.0 Prototype/task1.js
@@ -0,0 +1,26 @@
+let animal = {
+    name: "Cow",
+    walk() {
+        console.log("Animls can walk");
+    },
+    
+
+};
+
+let birds = {
+    legs: 2,
+    fly() {
+        console.log("Birds can fly");
+    },
+    __proto__: animal
+};
+
+let fish = {
+    fins: true,
+    swim() {
+        console.log("Fish can swim");
+    },
+    __proto__: 
+}
+
+console.log(birds.name);
