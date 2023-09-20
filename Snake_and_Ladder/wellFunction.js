7fdfa1abd830b4b8f0ce2ce66ae54cf968db7850
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-09-20 15:58:12 +0400

    Modify the code

--- Snake_and_Ladder/wellFunction.js
+++ Snake_and_Ladder/wellFunction.js
@@ -1,31 +1,18 @@
-
-// This is the Dice for the game
-
 function Dice() {
     return Math.floor(Math.random() * 6 + 1);
 }
-
 
 console.group("The Dice");
 console.log(Dice());
 console.groupEnd();
 
-// -----------------------------------------------------------------------------------------------
-
-
-// This is the snake and ladder for the board
-
 function snakeLadder() {
     const snakeAndLadder = {};
-
     let checkCondition = 0;
 
     for (let i = 0; i < 10; i++) {
-
         const head = Math.ceil(Math.random() * 99);
-
         const tail = Math.ceil(Math.random() * 99);
-
         checkCondition = (head !== tail);
 
         if (checkCondition) {
@@ -35,25 +22,14 @@ function snakeLadder() {
     return snakeAndLadder;
 }
 
-// -------------------------------------------------------------------------------------------------------------------
-
-
-// This gives the winner of the game
-
-function checkForWinner(player) {
-    if (player[player[i].length - 1] === 100) {
-
+function checkForWinner(player, i) {
+    if (player[i][player[i].length - 1] >= 100) { 
         return true;
     }
     return false;
 }
 
-
-// -------------------------------------------------------------------------------------------------------------------
-
-
 function playGame() {
-
     let dice = 0;
     let player = [[], [], [], []];
     const snakeAndLadder = snakeLadder();
@@ -62,33 +38,26 @@ function playGame() {
     console.table(snakeAndLadder);
     console.groupEnd();
 
-
     let winner = null;
 
-
     while (!winner) {
-
         for (let j = 0; j < player.length; j++) {
-
-            if (winner !== null) break; // If a winner is found, exit the loop.
+            if (winner !== null) break;
 
             let currentPosition = player[j].length ? player[j][player[j].length - 1] : 0;
 
             dice = Dice();
 
-            currentPosition = currentPosition + dice;
+            currentPosition += dice;
 
             if (snakeAndLadder[currentPosition]) {
-                // console.log("The players ladder position", currentPosition);
                 currentPosition = snakeAndLadder[currentPosition];
-                // console.log("The ladder of grace", currentPosition);
             }
 
             if (currentPosition <= 100) {
                 player[j].push(currentPosition);
 
-
-                if (checkForWinner(player[j])) {
+                if (checkForWinner(player, j)) {
 
                     console.group(`The winner is player ${j + 1}`);
                     console.log(player[j]);
