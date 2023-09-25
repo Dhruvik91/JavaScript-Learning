0c528e71f1147fd14159730fe38e3b41b4da6740
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-09-25 18:50:21 +0400

    Add the employee task

--- EmploymentManagement/emplymentdata.js
+++ EmploymentManagement/emplymentdata.js
@@ -1,3 +1,62 @@
 class Employees {
-    
+
+    constructor(email, personaInfo, performance) {
+
+        this.email = email;
+        this.personaInfo = personaInfo;
+        this.performance = performance
+    }
+
+    /* addEmployee(email, personaInfo, performance) {
+
+        return {
+            email,
+            personaInfo,
+            performance
+        }
+    } */
+};
+
+class PersonaInfo {
+
+    constructor(name, age, address) {
+
+        this.name = name;
+        this.age = age;
+        this.address = address
+    }
+};
+
+
+class Address {
+
+    constructor(street, city, state) {
+
+        this.street = street,
+            this.city = city,
+            this.state = state
+
+    }
 }
+
+class Performance {
+
+    constructor(quarterlyReview, annualReview) {
+        this.quarterlyReview = quarterlyReview;
+        this.annualReview - annualReview
+    
+    }
+}
+
+
+var address = new Address("123", "Gondal", "Gujarat");
+
+var personalInfo = new PersonaInfo("Dhruvik", "22", address);
+
+const performance = new Performance("Good", "Amzing");
+
+
+const employeeOne = new Employees("dhruvikgondaliya@gmail.com", personalInfo, performance);
+
+console.log(employeeOne);
+
