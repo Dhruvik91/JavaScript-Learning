eb2810b21be37006e536935b31ee397f8710d699
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-10-17 16:53:03 +0400

    Complete task of Currency Convertor

--- ConvertCurrency/code.js
+++ ConvertCurrency/code.js
@@ -6,47 +6,67 @@ Make necessary Assumptions */
 /* APIS from exchangerate*/
 
 
+
 async function currencyConvter(currencyFrom, currencyTo, amount) {
+
+
+    if (amount < 0 || isNaN(amount)) {
+
+        console.log(`Please enter the correct amount`);
+    }
 
     try {
 
-        const data = await validateResponse(currencyFrom);
-
-        const checkOtherCurrency = await validateResponse(currencyTo);
+        const base_url = 'https://v6.exchangerate-api.com/v6/'
+        const apiKey = 'ef18e086c2868fc97e360a12';
+        const getCurrency = `/pair/${currencyFrom}/${currencyTo}/${amount}`
+        const url = base_url + apiKey + getCurrency;
+        const response = await fetch(url);
+        const data = await response.json();
 
         console.log(data);
-        if (data.result === "success" && checkOtherCurrency.result === "success") {
 
-            const covertTo = data.conversion_rates[`${currencyTo}`];
-            const calculatedAmount = amount * covertTo;
-
-            console.log(calculatedAmount);
+        // (data);
+        if (data.result === "success") {
+            const covertTo = data.conversion_result;
+            console.log(covertTo);
         }
-        else {
-
-            data.result === "error" ? console.log(data['error-type']) : console.log(checkOtherCurrency['error-type'])
-
+        else if (data.result === "error") {
+            console.log(data['error-type']);
         }
 
     } catch (error) {
-
         console.log(error);
     }
 
 }
 
-currencyConvter("INR", 'USD', 'abc')
+(currencyConvter("USD", "INR", 6));
+
+/* async function wrapperFunction() {
+
+    let currencyfrom = document.getElementById("Cfirst").value;
+
+    console.log(currencyfrom);
+    let currencyto = document.getElementById("Csecond").value;
+
+    console.log(currencyto);
+    let amount = document.getElementById("amount").value;
+
+    console.log(amount);
 
 
-async function validateResponse(currency) {
+    document.getElementById("Cfirst").value = ' ';
+    document.getElementById("Csecond").value = ' ';
+    document.getElementById("amount").value = ' ';
 
-    const header = 'https://v6.exchangerate-api.com/v6/'
-    const apiKey = 'ef18e086c2868fc97e360a12';
-    const getCurrency = `/latest/${currency}`
-    const url = header + apiKey + getCurrency;
-    const response = await fetch(url);
-    const data = await response.json();
 
-    return data;
 
-}
+    const answer = await currencyConvter(currencyfrom, currencyto, amount)
+
+    console.log(answer);
+
+    document.getElementById("Display").innerHTML = answer;
+
+} */
+
