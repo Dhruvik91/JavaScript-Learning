4a5cf8a21540fadbb03906e2f51f13e1fa24a659
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-11-04 21:25:31 +0400

    Add the Game() function

--- SlotGame/index.js
+++ SlotGame/index.js
@@ -1,18 +1,56 @@
-const {
-    matchPattern,
-  } = require('./utils.js');
+const { matchPattern } = require('./utils.js');
+const prompt = require('prompt-sync')({ sigint: true });
 
-  function countMoney() {
+let totalMoney = 10000;
 
-    const matchedPatterns = matchPattern();
-    const countScore = matchedPatterns.reduce((acc, item) => acc + item.count, 0);
-    const money = countScore * 2;
-    return money;
+function countMoney() {
+  const matchedPatterns = matchPattern();
+  console.log(matchedPatterns);
+  const countScore = matchedPatterns.reduce((acc, item) => acc + item.count, 0);
+  const money = countScore * 2;
+  return money;
 }
 
-function Game() {
-    
-     
-  
+//-------------------------------------------------------------------------------------
 
+function checkBalance() {
+
+  if (totalMoney < 0) {
+    return ("Unsucifient Balance!!!");
+  }
+  else {
+    return (totalMoney);
+  }
 }
+
+//-------------------------------------------------------------------------------------
+
+function Game(bet) {
+
+  console.log("Please enter your choice:");
+  console.log("1. Roll");
+  console.log("2. Check Balance");
+  console.log("3. Add Balance");
+  console.log("4. Exit");
+
+  while (true) {
+
+    let choice = +prompt('What is the number?');
+    if (choice == 4) break;
+
+    switch (choice) {
+
+      case 1: {
+        totalMoney = totalMoney + countMoney() - bet;
+        console.log(totalMoney);
+      }
+        break;
+
+      case 2: console.log(checkBalance());
+        break;
+    }
+  }
+}
+
+Game(50000);
+
