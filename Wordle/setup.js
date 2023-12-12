d0df390733aabb04c380ad603a321af18e9c48b1
Dhruvik Gondaliya <yashgondaliya04@gmail.com>
2023-12-12 14:22:37 +0400

    Add set up for wordle game

---
+++ Wordle/setup.js
@@ -0,0 +1,57 @@
+/* This is the setup for the game wordle */
+
+const correctWord = "COVER";
+
+// function will verify the user entered word, if it exist or not
+async function verifyWord(wrd) {
+
+    try {
+        const BASE_URL = `https://api.dictionaryapi.dev`;
+        const response = await fetch(`${BASE_URL}/api/v2/entries/en/${wrd}`);
+        const wordData = await response.json();
+
+        if (wordData[0]?.word) {
+            return wrd;
+        } else if (wordData?.title) {
+            return 0;
+        }
+    } catch (err) {
+        console.error(err);
+    }
+};
+
+
+//function to make obj 
+function makeObjOf(obj, prop, item) {
+    return !obj[prop] ?
+        obj[prop] = [item] :
+        obj[prop].push(item);
+}
+
+// function to verify the available letters in the entered word
+function verifyIndicsOfLetters(userWord) {
+    const checkedLettersPosition = {};
+
+    for (let i = 0; i < userWord.length; i++) {
+
+        if (correctWord.indexOf(userWord[i]) === userWord.indexOf(userWord[i])) {
+            makeObjOf(checkedLettersPosition, "letterInPerfectPosition", userWord[i])
+
+        } else if (correctWord.indexOf(userWord[i]) !== userWord.indexOf(userWord[i])
+            && correctWord.indexOf(userWord[i]) !== -1) {
+            makeObjOf(checkedLettersPosition, "letterInImperfectPosition", userWord[i])
+
+        } else {
+            makeObjOf(checkedLettersPosition, "letterNotInWord", userWord[i])
+        }
+    }
+    return checkedLettersPosition;
+}
+
+module.export = {
+    correctWord,
+    verifyIndicsOfLetters
+};
+
+
+
